3bb266c4a54261f5f0f363ade3ce33ab891af0a1
BoeingX <song.baoyang@gmail.com>
2016-05-02 10:42:45 +0800

    add shortest path search; allow case insensative search and auto substitute space by underline

--- wiki.py
+++ wiki.py
@@ -9,6 +9,7 @@ from wtforms.validators import Required
 from py2neo import authenticate, Graph
 from datetime import timedelta
 from flask import session, app
+import re
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -19,29 +20,29 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 neo4j_user = os.environ.get('NEO4J_USER') or 'neo4j'
-neo4j_pass = os.environ.get('NEO4J_PASS') or ''
+neo4j_pass = os.environ.get('NEO4J_PASS') or 'neo4j'
 authenticate("localhost:7474", neo4j_user, neo4j_pass)
 graph = Graph()
 
 def get_neighbors(graph, name = None, n_neighbours = 20):
     if name is None:
-        results = graph.cypher.execute("MATCH (p:Article)-[:TO_ARTICLE]->(q:Article) RETURN q.name as name LIMIT %d" % n_neighbours) 
+        results = graph.cypher.execute("MATCH (p:Article)-[:TO_ARTICLE]->(q:Article) RETURN p.name as root, q.name as name LIMIT %d" % n_neighbours) 
     else:
-        results = graph.cypher.execute("MATCH (p:Article{name:'%s'})-[:TO_ARTICLE]->(q:Article) RETURN q.name as name LIMIT %d" % (name, n_neighbours))
+        results = graph.cypher.execute("MATCH (p:Article)-[:TO_ARTICLE]->(q:Article) WHERE p.name =~ '(?i)%s' RETURN p.name as root, q.name as name LIMIT %d" % (name, n_neighbours))
     return results
 
-def get_path(name1, name2, depth = 5):
-    return
+def get_path(graph, name1, name2, depth = 5):
+    result = graph.cypher.execute("MATCH s = shortestPath((p:Article)-[:TO_ARTICLE*1..%d]->(q:Article)) WHERE p.name =~ '(?i)%s' and q.name =~ '(?i)%s' RETURN s as path LIMIT 1" % (depth, name1, name2))
+    return result
 
 class NameForm(Form):
-    name = StringField('Root page (e.g. Hidden_Markov_model)?', validators=[Required()])
+    name = StringField('Root page (e.g. Hidden Markov model)?', validators=[Required()])
     submit = SubmitField('Submit')
 
-#@app.before_request
-#def make_session_permanent():
-#    session.permanent = True
-#    app.permanent_session_lifetime = timedelta(seconds = 10)
-
+class PathForm(Form):
+    name1 = StringField('First page (e.g. Hidden Markov model)?', validators=[Required()])
+    name2 = StringField('First page (e.g. Support vector machine)?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -61,13 +62,13 @@ def neighbors():
     form = NameForm()
     nodes = get_neighbors(graph)
     if form.validate_on_submit():
-        name = form.name.data
+        name = re.sub(' ', '_', form.name.data)
         neighbors = get_neighbors(graph, name).records
         if len(neighbors) == 0:
             flash(Markup('Root node not found!'))
             return redirect(url_for('neighbors'))
         else:
-            session['root'] = name
+            session['root'] = neighbors[0].root
             session['neighbors'] = [neighbor.name for neighbor in neighbors]
             return redirect(url_for('neighbors_result'))
     return render_template('neighbors.html', form = form)
@@ -76,7 +77,26 @@ def neighbors():
 def neighbors_result():
     root = session.get('root')
     neighbors = session.get('neighbors')
+    if root is None or neighbors is None:
+        return render_template('404.html'), 404
     return render_template('neighbors_result.html', root = root, neighbors = neighbors)
+
+@app.route('/path', methods=['GET', 'POST'])
+def path():
+    form = PathForm()
+    if form.validate_on_submit():
+        name1 = re.sub(' ', '_', form.name1.data)
+        name2 = re.sub(' ', '_', form.name2.data)
+        path = get_path(graph, name1, name2).records
+        if len(path) == 0:
+            flash(Markup('Path not found!'))
+            return render_template('neighbors.html', form = form)
+        else:
+            path = [node['name'] for node in path[0].path.nodes]
+            print path
+            return redirect(url_for('index'))
+    return render_template('neighbors.html', form = form)
+
 
 if __name__ == '__main__':
     manager.run()
